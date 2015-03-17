39296f6588b1b152043523eadfb6d2d8348dc7fa
SharpCoder <colemangrill@live.com>
2015-03-17 09:06:24 -0700

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# node-moby
-An npm module for accessing the moby linguistic package.
+# About the project
+This is an npm module for accessing the moby parts-of-speech package. Moby is an open source project with many linguistic databases (all public domain). You can access more about their project by visiting the following URL: http://icon.shef.ac.uk/Moby/
+
+##Using moby-pos
+
+This npm module aims to be pretty simple to use. It has a single method which you can call to look up the parts of speech information for a given word.
+'''javascript
+    var pos = require('moby-pos');
+    console.log(pos.search("skeleton"));
+'''
+
+##Reading the output
+
+The output of the above code should be **N**. That stands for Noun. The complete list of meanings can be found below.
+
+Meaning | Letter
+------- | -------
+Noun | N
+Plural | P
+Noun Phrase | h
+Verb (usu participle) | V
+Verb (transitive) | t
+Verb (intransitive) | i
+Adjective | A
+Adverb | v
+Conjunction | C
+Preposition | P
+Interjection | !
+Pronoun | r
+Definite Article | D
+Indefinite Article | I
+Nominative | o
---
+++ index.js
@@ -0,0 +1,53 @@
+// Created by SharpCoder
+//
+// This project aims to open up multiple
+// linguistic functions available from the
+// moby project. More information available
+// here: http://icon.shef.ac.uk/Moby/
+
+(function() {
+
+  var fs = require('fs');
+  var dictionary = {};
+  var file = './mobypos.txt';
+
+  // Read the file (synchronously, for now).
+  var data = fs.readFileSync(file).toString("utf8");
+
+  // Get some parse variables.
+  var word = '', type = '';
+  var half = false;
+
+  // Parse the file into the dictionary.
+  for ( var i = 0; i < data.length; i++ ) {
+
+    // Get the character we're parsing.
+    var c = data.charCodeAt(i);
+
+    // Now figure out what to do with it.
+    if ( c == 215 )
+    {
+      half = true;
+    }
+    else if ( c == 13 )
+    {
+      dictionary[word] = type;
+      word = '';
+      type = '';
+      half = false;
+    }
+    else if ( half )
+    {
+      type += data[i];
+    }
+    else
+    {
+      word += data[i];
+    }
+  }
+
+  exports.search = function(val) {
+    return dictionary[val];
+  };
+
+})();
